69e8f91715eb2963daaf18ac272d70850713d79f
Derkse14 <daniel.dderksen@gmail.com>
2016-11-14 17:25:54 -0600

    Finished Menu Generator

--- WebContent/MenuGenerator.js
+++ WebContent/MenuGenerator.js
@@ -33,8 +33,15 @@ var menu = new Menu
             		new MenuItem
             		(
             				"Spinach & Swiss Omelette",
-            				"Swiss cheese, sautÃ©ed baby spinach, red onion, tomato and mushrooms finished with creamy hollandaise sauce.",
+            				"Swiss cheese, sauteed baby spinach, red onion, tomato and mushrooms finished with creamy hollandaise sauce.",
             				14.69,
+            				null
+            		),
+            		new MenuItem
+            		(
+            				"Original French Toast",
+            				"Three slices of French bread dipped in our original cinnamon and nutmeg mixture, grilled golden brown and sprinkled with powdered sugar.",
+            				10.59,
             				null
             		)]
             ),
@@ -55,6 +62,132 @@ var menu = new Menu
             				11.99,
             				["Chipotle", "Marinara", "Ranch"]
             		)]
+            ),
+            new Category
+            (
+            		"Soups",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Salads",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Steaks",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Sandwiches",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Pasta",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Beverages",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
+            ),
+            new Category
+            (
+            		"Desserts",
+            		[new MenuItem
+            		(
+            				"Wings1",
+            				"Lightly breaded and fried chicken wings",
+            				10.99,
+            				["Salt & Pepper", "Buffalo", "Honey Garlic"]
+            		),
+            		new MenuItem
+            		(
+            				"Southwest Flatbread1",
+            				"Diced BBQ chicken, bacon and red onions baked on our flatbread with sweet and smoky BBQ sauce and pizza mozzarella, then topped with a drizzle of ranch dressing and a sprinkling of fresh parsley.",
+            				11.99,
+            				["Chipotle", "Marinara", "Ranch"]
+            		)]
             )
 ]);
 
@@ -62,16 +195,19 @@ var menu = new Menu
 function generateMenu() {
 	for(i = 0; i < menu.categories.length; i++) {
 		var category = menu.categories[i];
-		var li = $("<li>", {"class" : category.name});
-		li.html(category.name);
-		$(".menu").append(li);
-		var ul = $("<ul>");
-		$("."+category.name).append(ul);
+		var catDiv = $("<div>", {"id" : category.name.replace(/ /g,'')});
+		$(".menu").append(catDiv);
+		$("#"+category.name).append($("<h2>", {"class" : "categoryName"}).text(category.name));
 		for(j = 0; j < category.items.length; j++) {
 			var item = category.items[j];
-			var li = $("<li>");
-			li.html(item.name);
-			$("."+category.name + " ul").append(li);
+			var itemClass = item.name.replace(/ /g,'');
+			itemClass = itemClass.replace('&','');
+			var itemDiv = $("<div>", {"class" : itemClass+" Item"});
+			$("#"+category.name).append(itemDiv);
+			$("."+itemClass).append($("<h4>", {"class" : "price"}).text(item.price));
+			$("."+itemClass).append($("<h3>", {"class" : "itemName"}).text(item.name));
+			$("."+itemClass).append($("<p>", {"class" : "description"}).text(item.description));
+			
 		}
 	}
 }
