d73db708db9f66d7f9bf171573b800970087aef3
Shamalka Herath <umherat2@myumanitoba.ca>
2016-11-20 17:20:57 -0600

    completed confirmation dialog

--- WebContent/MenuGenerator.js
+++ WebContent/MenuGenerator.js
@@ -41,11 +41,15 @@ function generateDetailedView(name) {
 	$(".menu").hide();
 	$(".detailedView").css('visibility', 'visible');
 	var item = getItem(name);
+	currentItem = item;
 	if(item == null) {
 		alert("not found");
 	} else {
 //		alert(item.name);
 	}
+
+	var cost = item.price;
+
 	$(".headerTitle").text(item.name);
 	$(".headerTitle").css("font-size", "4em");
 	$(".detailedView").append($("<img>", {"src" : "../Images/"+item.image}));
@@ -54,20 +58,80 @@ function generateDetailedView(name) {
 	
 	if(item.selections != null) {
 		for(var i = 0; i < item.selections.length; i++) {
-			var select = $("<select>", {"id" : item.selections[i].selection_id});
-
 			$(".detailedView .selections").append($("<label>").text(item.selections[i].selection_desc));
+			var select = $("<select>", {"id" : item.selections[i].selection_id, "class": "formRow"});		
 			$(".detailedView .selections").append(select);
+			
 			for(var j = 0; j < item.selections[i].options.length; j++) {
 				var option = item.selections[i].options[j].option_desc;
 				$("#"+item.selections[i].selection_id).append($("<option>", {value: option, text: option}));
+			}
+
+			if(i<(item.selections.length-1)){ 
+				var br = document.createElement("br");
+				$(".detailedView .selections").append($(br));  
 			}
 			
 		}
 	}
 
-	$(".detailedView").append($("<label>", {"class" : "backButton"}).text("Go back"));
+	$(".detailedView").append($("<label>", {"class": "labelWidth"}).text("Customer Name: "));
+	$(".detailedView").append($("<input>", {"class": "formRow", "type" : "text", "name": "cusName"}));
+	var br = document.createElement("br");
+	$(".detailedView").append($(br));  
+	$(".detailedView").append($("<label>", {"class": "labelWidth"}).text("Allergies: "));
+	$(".detailedView").append($("<input>", {"class": "formRow","type" : "text", "name": "allergies"}));
+	var br = document.createElement("br");
+	$(".detailedView").append($(br)); 
+	$(".detailedView").append($("<label>", {"class": "labelWidth"}).text("Cost: "));
+	$(".detailedView").append($("<label>", {"class": "formRow"}).text("$ "+cost));
+	var br = document.createElement("br");
+	$(".detailedView").append($(br)); 
+	$(".detailedView").append($("<button>", {"class" : "backButton"}).text("Go back"));  
+	var btnConfirmOrder = document.createElement("button");
+	btnConfirmOrder.setAttribute("class", "addToOrder"); 
+	btnConfirmOrder.innerHTML = "Add To Order";  
+	$(".detailedView").append(btnConfirmOrder);
 	
+	btnConfirmOrder.onclick = function(){
+		var cusItem = {}; 
+		var cusName = document.getElementsByName('cusName')[0].value; 
+		cusItem.cusName = cusName; 
+		var info = "Customer Name: "+ cusName + "\n\n"; 
+		var allergies = document.getElementsByName('allergies')[0].value ; 
+		cusItem.allergies = allergies; 
+		info = info + " Allergies: "+ allergies+ "\n\n"; 
+
+		info = info + " Item: "+item.name+"\n\n"; 
+		var cost = parseFloat(item.price); 
+		selectedOptions = []; 
+		for(var i = 0; i < item.selections.length; i++) {
+			var option = $("#"+item.selections[i].selection_id).val(); 
+			selectedOptions[i] = option; 
+			var index = 0; 
+			var options = item.selections[i].options; 
+			var found = false; 
+			while(index < options.length && !found){
+				var tempOption = options[index]; 
+				if(tempOption.option_desc == option){
+					found = true; 
+				}else{
+					index++; 
+				}
+				
+			}
+			cost = cost + parseFloat(item.selections[i].options[index].option_price); 
+		}
+
+		info = info + "Cost: "+ cost+"\n\n"; 
+		cost = 0; 
+
+		var confirmedOrder = confirm(info); 
+		if(confirmedOrder == true){
+			//doing something
+		 
+		}
+	}
 	
 }
 
