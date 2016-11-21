a5bd1d97d4e68d2946ba7b42f234903d70d08ac2
Shamalka Herath <umherat2@myumanitoba.ca>
2016-11-21 00:38:43 -0600

    included item links in list of orders, alert to help

--- WebContent/MenuGenerator.js
+++ WebContent/MenuGenerator.js
@@ -79,12 +79,19 @@ function generateOrderSidebar() {
 		var custDiv = $("<div>").text(cusOrderList[ii].customer);
 		var itemList = $("<ul>");
 		for(jj = 0; jj < cusOrderList[ii].items.length; jj++) {
+			var args = [cusOrderList[ii].customer, cusOrderList[ii].items[jj].item, cusOrderList[ii].allergies]; 
 			var orderedItem = $("<a>", {"href": "Menu.html#"}).text(cusOrderList[ii].items[jj].item.name);
+			var br = document.createElement("br");
+			orderedItem.append(br);
 			itemList.append(orderedItem);
 		}
 		custDiv.append(itemList);
 		$(".orderSidebar").append(custDiv);
 	}
+}
+
+function getHelp(){
+	alert("A server will be there to assist you!");  
 }
 
 function generateMenu() {
